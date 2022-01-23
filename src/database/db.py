433c42e45cfbe6a1513933d0ccf71f227ae7b201
failip <philipp@kuboth.dev>
2022-01-23 18:47:39 +0100

    Moved balance updating into one function

--- src/database/db.py
+++ src/database/db.py
@@ -1,29 +1,15 @@
 from pymongo import MongoClient
-import datetime
-client = MongoClient('localhost', 27017, username="root", password="root")
-
-db = client.test_database
-post = {"author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.datetime.utcnow()}
-
-posts = db.posts  # get posts
-
-post_id = posts.insert_one(post).inserted_id
-# collection = db.test_collection
 
 
 class Database:
     def __init__(self):
         self.client = MongoClient(
             'localhost', 27017, username="root", password="root")
-        self.db = client.test_database
-        self.users = db.users
+        self.db = self.client.test_database
+        self.users = self.db.users
 
     def get_all_users(self):
         pass
-        # self.all_users=
 
     def create_user(self, user):
         if not self.user_exsists(user):
@@ -39,17 +25,17 @@ class Database:
         print("User does not exsist")
         return
 
-    def change_balance(self, user, balance):
-        pass
+    def update_balance(self, user, balance):
+        self.users.find_one_and_update(
+            {"name": user.name}, {"$inc": {"balance": balance}})
 
     def increase_balance(self, user, amount):
-        updated_balance = self.users.find_one_and_update(
-            {"name": user.name}, {"$inc": {"balance": amount}})
-        print("Updated Balance: ")
-        return
+        user.balance += amount
+        self.update_balance(user)
 
     def decrease_balance(self, user, amount):
-        pass
+        user.balance -= amount
+        self.update_balance(user)
 
     def user_exsists(self, user):
         if list(self.users.find({"name": user.name})):
