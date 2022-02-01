d96005067efbaba7ed654ef132d68e95b48b916c
failip <philipp@kuboth.dev>
2022-02-01 19:40:09 +0100

    Removed test stuff

--- src/CoffeeOrg.py
+++ src/CoffeeOrg.py
@@ -13,21 +13,3 @@ for user2 in database.users.find({}):
 print("After changes:")
 for user2 in database.users.find({}):
     print(user2)
-
-
-def read_yaml():
-    with open("../resources/prices.yaml", "r") as f:
-        try:
-            prices = yaml.safe_load(f)
-            return prices
-        except yaml.YAMLError as exc:
-            print(exc)
-
-
-def buy(user, something):
-    prices_dict = read_yaml()
-    price_of_something = prices_dict["prices"][something][0]
-    database.decrease_balance(user, price_of_something)
-
-
-buy(database.get_user("Jan"), "beer")
